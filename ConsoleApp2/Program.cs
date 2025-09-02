58a9a8485ba11c69d6b9ac860dc24d2561a2238e
Andy241063 <andrefonseca.info@gmail.com>
2025-09-02 16:04:41 -0300

    Update Program.cs
    Segunda modificacao

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,2 +1,2 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World! I am here !");
+Console.WriteLine("Hello, World! I am here ! Tell me what you want !");
